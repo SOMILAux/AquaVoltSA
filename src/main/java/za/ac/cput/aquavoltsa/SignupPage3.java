18b440173b96588c2eb15ab1a1ba762b150968b6
Oyama Sibunzi <yamasibunzi@gmail.com>
2026-05-20 19:28:46 +0200

    This is the Third page that will appear when the mobile app is running/ opened, this is the second part for the sign-up page. This page contains coding for the Sign-up part 2 processes.

---
+++ src/main/java/za/ac/cput/aquavoltsa/SignupPage3.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.ac.cput.aquavoltsa;
+import javax.swing.*;
+    import java.awt.*;
+    import java.awt.event.*;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+
+
+public class SignupPage3 extends JFrame {
+    public SignupPage3() {
+        setTitle("AquaVolt SA - Sign-up Page Part 3");
+        setSize(425, 917);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new GridLayout(12, 1, 10, 10));
+
+        JCheckBox terms1 = new JCheckBox("I read and agree to these terms and conditions");
+        JCheckBox terms2 = new JCheckBox("I read and agree to these terms and conditions");
+
+        JButton termsBtn = new JButton("Terms and Condition...");
+        JButton enterBtn = new JButton("Enter");
+        JButton backBtn = new JButton("Back");
+
+        add(new JLabel("Electricity Details", SwingConstants.CENTER));
+        add(new JTextField("Enter Home Address"));
+        add(new JTextField("Enter Meter Number"));
+        add(new JTextField("Enter Serial Number"));
+        add(new JLabel("*You can skip this process and update it later on", SwingConstants.CENTER));
+        add(termsBtn);
+        add(terms1);
+        add(terms2);
+        add(enterBtn);
+        add(backBtn);
+
+        termsBtn.addActionListener(e -> {
+            new TermsPage().setVisible(true);
+            dispose();
+        });
+
+        backBtn.addActionListener(e -> {
+            new SignupPage2().setVisible(true);
+            dispose();
+        });
+        
+    }
+}
